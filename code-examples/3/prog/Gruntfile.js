5fb257bd5a5163edc1d37d29b4c6875d2915a908
Jaime Pillora <jpillora@gmail.com>
2013-08-14 18:12:18 +1000

    ch3 examples

---
+++ code-examples/3/prog/Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  grunt.registerTask("foo", function() {
+    console.log(0);
+    grunt.task.run("bar");
+    grunt.task.run("bazz");
+  });
+
+  grunt.registerTask("bar", function() {
+    console.log(1);
+  });
+
+  grunt.registerTask("bazz", function() {
+    console.log(2);
+    grunt.task.run(["ping", "pong"]);
+  });
+
+  grunt.registerTask("ping", function() {
+    console.log(3);
+  });
+
+  grunt.registerTask("pong", function() {
+    console.log(4);
+  });
+
+  // Define the default task
+  grunt.registerTask('default', ['foo']);
+};
