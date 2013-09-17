7345d73142fd364ae63cc4b6c5bea6624c9e6421
Jaime Pillora <jpillora@gmail.com>
2013-09-17 23:40:57 +1000

    added watch example

--- code-examples/3/13-watch/Gruntfile.js
+++ code-examples/3/13-watch/Gruntfile.js
@@ -1,34 +1,27 @@
 module.exports = function(grunt) {
 
-  grunt.registerTask('foo', function() {
-    console.log(0);
-    grunt.task.run('bar');
-    grunt.task.run('bazz');
-    grunt.task.run('bang');
-  });
+  // Load the plugins that provide the "concat" and "watch" tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('bar', function() {
-    console.log(1);
+  // Project configuration.
+  grunt.initConfig({
+    srcFiles: ["src/a.js", "src/b.js", "src/c.js"],
+    concat: {
+      target1: {
+        files: {
+          "build/abc.js": "<%= srcFiles %>"
+        }
+      }
+    },
+    watch: {
+      target1: {
+        files: "<%= srcFiles %>",
+        tasks: ["concat"]
+      }
+    }
   });
-
-  grunt.registerTask('bazz', function() {
-    console.log(2);
-    grunt.task.run(['ping', 'pong']);
-  });
-
-  grunt.registerTask('ping', function() {
-    console.log(3);
-  });
-
-  grunt.registerTask('pong', function() {
-    console.log(4);
-  });
-
-  grunt.registerTask('bang', function() {
-    console.log(5);
-  });
-
 
   // Define the default task
-  grunt.registerTask('default', ['foo']);
+  grunt.registerTask('default', ['concat', 'watch']);
 };
