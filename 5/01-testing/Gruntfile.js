550c6fede9580f0b5f3dd522aa8fab9297502186
Jaime Pillora <jpillora@gmail>
2013-11-18 11:18:48 +1100

    updated

---
+++ 5/01-testing/Gruntfile.js
@@ -0,0 +1,18 @@
+
+
+module.exports = function(grunt) {
+
+  //test
+  grunt.initConfig({
+    mocha: {
+      myFirstTest: {
+        src: 'test/my-first-test.html'
+      }
+    }
+  });
+
+
+
+
+
+};
